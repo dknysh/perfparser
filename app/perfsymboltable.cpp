7a36dedff4c7c2d4a52d33fe47ae46975873fc10
dknysh <d.knysh@nips.ru>
2020-10-23 13:30:56 +0700

    Added actual file path computation for symbols.
    Actual file path can differ from perf recorded.
    Compute and pass it to hotspot through Symbol struct field actualPath.

--- app/perfsymboltable.cpp
+++ app/perfsymboltable.cpp
@@ -336,7 +336,7 @@ int PerfSymbolTable::insertSubprogram(CuDieRangeMapping *cudie, Dwarf_Die *top, 
     return locationId;
 }
 
-int PerfSymbolTable::parseDie(CuDieRangeMapping *cudie, Dwarf_Die *top, quint64 offset, quint64 size, quint64 relAddr, qint32 binaryId, qint32 binaryPathId,
+int PerfSymbolTable::parseDie(CuDieRangeMapping *cudie, Dwarf_Die *top, quint64 offset, quint64 size, quint64 relAddr, qint32 binaryId, qint32 binaryPathId, qint32 actualPathId,
                               bool isKernel, Dwarf_Files *files, Dwarf_Addr entry, qint32 parentLocationId)
 {
     int tag = dwarf_tag(top);
@@ -370,7 +370,7 @@ int PerfSymbolTable::parseDie(CuDieRangeMapping *cudie, Dwarf_Die *top, quint64 
 }
 
 qint32 PerfSymbolTable::parseDwarf(CuDieRangeMapping *cudie, SubProgramDie *subprogram, const QVector<Dwarf_Die> &inlined,
-                                   Dwarf_Addr bias, quint64 offset, quint64 size, quint64 relAddr, qint32 binaryId, qint32 binaryPathId, bool isKernel)
+                                   Dwarf_Addr bias, quint64 offset, quint64 size, quint64 relAddr, qint32 binaryId, qint32 binaryPathId, qint32 actualPathId, bool isKernel)
 {
     Dwarf_Files *files = nullptr;
     dwarf_getsrcfiles(cudie->cudie(), &files, nullptr);
@@ -382,7 +382,7 @@ qint32 PerfSymbolTable::parseDwarf(CuDieRangeMapping *cudie, SubProgramDie *subp
         if (dwarf_entrypc(&scope, &entry) == 0 && entry != 0)
             scopeAddr += entry;
 
-        auto locationId = parseDie(cudie, &scope, offset, size, relAddr, binaryId, binaryPathId, isKernel, files, scopeAddr, parentLocationId);
+        auto locationId = parseDie(cudie, &scope, offset, size, relAddr, binaryId, binaryPathId, actualPathId, isKernel, files, scopeAddr, parentLocationId);
         if (locationId != -1)
             parentLocationId = locationId;
     };
@@ -766,10 +766,14 @@ int PerfSymbolTable::lookupFrame(Dwarf_Addr ip, bool isKernel,
 
     qint32 binaryId = -1;
     qint32 binaryPathId = -1;
+    qint32 actualPathId = -1;
     quint64 elfStart = 0;
+    QFileInfo actualPath;
     if (elf.isValid()) {
         binaryId = m_unwind->resolveString(elf.originalFileName);
         binaryPathId = m_unwind->resolveString(elf.originalPath);
+        actualPath = findFile(elf.originalPath, elf.originalFileName, {});
+        actualPathId = m_unwind->resolveString(actualPath.filePath().toUtf8());
         elfStart = elf.hasBaseAddr() ? elf.baseAddr : elf.addr;
     }
 
@@ -842,7 +846,7 @@ int PerfSymbolTable::lookupFrame(Dwarf_Addr ip, bool isKernel,
                     // otherwise resolve the inline chain if possible
                     if (!scopes.isEmpty() && !m_unwind->hasSymbol(addressLocation.parentLocationId)) {
                         functionLocation.parentLocationId = parseDwarf(cudie, subprogram, scopes, bias, start, size, relAddr,
-                                                                       binaryId, binaryPathId, isKernel);
+                                                                       binaryId, binaryPathId, actualPathId, isKernel);
                     }
                 }
             }
@@ -860,7 +864,7 @@ int PerfSymbolTable::lookupFrame(Dwarf_Addr ip, bool isKernel,
             // no sufficient debug information. Use what we already know
             qint32 symId = m_unwind->resolveString(symname);
             m_unwind->resolveSymbol(addressLocation.parentLocationId,
-                                    PerfUnwind::Symbol(symId, start, size, binaryId, binaryPathId, isKernel));
+                                    PerfUnwind::Symbol(symId, start, size, binaryId, binaryPathId, actualPathId, isKernel));
         }
     } else {
         if (isKernel) {
@@ -881,7 +885,7 @@ int PerfSymbolTable::lookupFrame(Dwarf_Addr ip, bool isKernel,
         if (!m_unwind->hasSymbol(addressLocation.parentLocationId)) {
             qint32 symId = m_unwind->resolveString(symname);
             m_unwind->resolveSymbol(addressLocation.parentLocationId,
-                                    PerfUnwind::Symbol(symId, start, size, binaryId, binaryPathId, isKernel));
+                                    PerfUnwind::Symbol(symId, start, size, binaryId, binaryPathId, actualPathId, isKernel));
         }
     }
     // relAddr - relative address of the function start added with offset from the function start
